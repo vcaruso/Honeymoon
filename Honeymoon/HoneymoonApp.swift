5ba5ff3ee39db6cb4336032d4d3b0af8fc62f128
Vincenzo Caruso <enzo@carusovincenzo.com>
2022-07-31 16:44:44 +0200

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Vincenzo Caruso on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
